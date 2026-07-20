66dceda2c2aeff6686635789922fb2c130378e26
kajol joby <kajoljoby@gmail.com>
2026-07-21 01:05:18 +0530

    Added Firebase configuration

---
+++ firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDlHRhU1irzizI-ebgQ5hca35fYODibsD4",
+  authDomain: "smart-expiry-tracker-c1cb5.firebaseapp.com",
+  databaseURL: "https://smart-expiry-tracker-c1cb5-default-rtdb.firebaseio.com",
+  projectId: "smart-expiry-tracker-c1cb5",
+  storageBucket: "smart-expiry-tracker-c1cb5.firebasestorage.app",
+  messagingSenderId: "84486894172",
+  appId: "1:84486894172:web:686d3850e02ada0a752c99",
+  measurementId: "G-M9WL2KY46N"
+};
